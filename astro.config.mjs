62d9bd7533d4a585dbc78d0b516ff8c6660b14b0
balamurugan16 <balamurugan.07@outlook.com>
2023-09-03 18:13:26 +0530

    gonna deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	site: 'https://astro-frontend-masters.netlify.app',
-	integrations: [react(), solidJs()],
+  output: 'hybrid',
+  site: 'https://astro-frontend-masters.netlify.app',
+  integrations: [react(), solidJs()],
+  adapter: netlify()
 });
